336956f65e06eaf76c37dc9885f159cd39e19bb6
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2020-12-10 00:09:20 -0500

    Measure from multiple countries consecutively and store the output in a dictionary of dictionaries

--- modules/measurements-client/MeasurementsClient.py
+++ modules/measurements-client/MeasurementsClient.py
@@ -19,24 +19,28 @@ with open('config.json', 'r') as f:
 #Get variables for StoreScp from config.json.
 target = config['Target']
 no_of_probes = config['NoOfProbes']
-from_country = config['From']
+from_countries = config['From']
 measure = config['Measure']
 packets = config['Packets']
+me = config['Me']
 
 logging.basicConfig(filename='sintra.csv', level=logging.INFO, format='%(message)s')
 
-latency_dict = dict()
-
-ripe = subprocess.run("ripe-atlas measure {0} --target {1} --probes {2} --from-country {3} --packets {4}".format(measure, target, no_of_probes, from_country, packets), capture_output=True, shell=True, encoding="utf8")
-
-output_str = ripe.stdout
-
-output_line_separated = output_str.split('\n')
+whole_dict = dict()
 
 
-for line in output_line_separated:
-	if len(line) > 1:
-	    entries = line.split()
-	    latency_dict[entries[5]] = entries[9][4:]
 
-logging.info(latency_dict)
+for country in from_countries:
+    each_dict = dict()
+    ripe = subprocess.run("ripe-atlas measure {0} --target {1} --probes {2} --from-country {3} --packets {4}".format(measure, target, no_of_probes, country, packets), capture_output=True, shell=True, encoding="utf8")
+    output_str = ripe.stdout
+    output_line_separated = output_str.split('\n')
+
+
+    for line in output_line_separated:
+	    if len(line) > 1:
+	        entries = line.split()
+	        each_dict[entries[5]] = entries[9][4:]
+    whole_dict[country] = each_dict
+
+logging.info(whole_dict)
--- modules/measurements-client/README.md
+++ modules/measurements-client/README.md
@@ -1,3 +1,21 @@
 # Sintra Measurements Client
 
 Sintra Measurements Client is a python-based toolkit that performs periodic Internet measurements based on RIPE Atlas.
+
+
+# Configuring Sintra Measurements Client
+
+Sintra Measurements Client performs the measurements based on the RIPE Atlas Client.
+
+Find the config.json file in the folder and modify accordingly.
+
+* *Target*: The target that is set to receive the data from the probes.
+
+* *NoOfProbes*: Maximum is 500 per iteration. You may leave this value as-is.
+
+* *From*: An array of nations to send data from.
+
+* *Measure*: Measurement. For latency, leave it as the default value, "ping".
+
+* *Me*: Set your or your probe's IP address. 
+
