31d0345462a754318773d866a70921453c853d9b
Sai Vamsi Alisetti <vamsi@Vamsis-MacBook-Pro.local>
2024-06-18 09:56:58 +0530

    adding simple 2way topology

--- modules/emulator/topology.py
+++ modules/emulator/topology.py
@@ -5,15 +5,16 @@ class NetworkTopology(Topo):
         # Initialize topology
         Topo.__init__( self )
         # Add hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        # Add switches
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
+        h1 = self.addHost('origin')
+
+        h2 = self.addHost('intermediate_node1')
+        h3 = self.addHost('intermediate_node2')
+        h4 = self.addHost('destination')
+
         # Add links
-        self.addLink(h1,s1)
-        self.addLink(h2,s1)
-        self.addLink(h1,s2)
-        self.addLink(h2,s2)
+        self.addLink(h1, h2)
+        self.addLink(h1, h3)
+        self.addLink(h4, h3)
+        self.addLink(h4, h2)
 
 topos = { 'network_topology': (lambda: NetworkTopology())}
