6faa6e932a3ff86333a6c051e13a23a8ab716a15
Vamsi995 <saivamsi.ds123@gmail.com>
2024-06-18 16:58:07 +0530

    adding sdn controller

---
+++ modules/emulator/controller.py
@@ -0,0 +1,8 @@
+
+from ryu.base import app_manager
+
+class SimpleSwitch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch, self).__init__(*args, **kwargs)
