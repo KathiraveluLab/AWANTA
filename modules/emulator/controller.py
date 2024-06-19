78be944cec036584f55d6340e38d27d7d5da68fd
Vamsi995 <saivamsi.ds123@gmail.com>
2024-06-19 17:17:33 +0530

    ryu controller setup

--- modules/emulator/controller.py
+++ modules/emulator/controller.py
@@ -1,8 +1,52 @@
 
 from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
 
-class SimpleSwitch(app_manager.RyuApp):
+
+class Controller(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch, self).__init__(*args, **kwargs)
+        super(Controller, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Add table miss flow entries
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+
+        self.logger.info('OFPSwitchFeatures received: '
+                          'datapath_id=0x%016x n_buffers=%d '
+                          'n_tables=%d auxiliary_id=%d '
+                          'capabilities=0x%08x',
+                          ev.msg.datapath_id, ev.msg.n_buffers, ev.msg.n_tables,
+                          ev.msg.auxiliary_id, ev.msg.capabilities)
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+
+        datapath.send_msg(mod)
+
--- modules/emulator/topology.py
+++ modules/emulator/topology.py
@@ -4,17 +4,22 @@ class NetworkTopology(Topo):
     def __init__( self ):
         # Initialize topology
         Topo.__init__( self )
-        # Add hosts
-        h1 = self.addHost('origin')
 
-        h2 = self.addHost('intermediate_node1')
-        h3 = self.addHost('intermediate_node2')
-        h4 = self.addHost('destination')
+        # Add hosts
+        alaska_host = self.addHost('h1')
+        newyork_host = self.addHost('h2')
+
+        # Add Switches
+        alaska_switch = self.addSwitch('s1')
+        seattle_switch = self.addSwitch('s12')
+        newyork_switch = self.addSwitch('s2')
+
 
         # Add links
-        self.addLink(h1, h2)
-        self.addLink(h1, h3)
-        self.addLink(h4, h3)
-        self.addLink(h4, h2)
+        self.addLink(alaska_host, alaska_switch)
+        self.addLink(alaska_switch, seattle_switch)
+        self.addLink(seattle_switch, newyork_switch)
+        self.addLink(alaska_switch, newyork_switch)
+        self.addLink(newyork_switch, newyork_host)
 
 topos = { 'network_topology': (lambda: NetworkTopology())}
