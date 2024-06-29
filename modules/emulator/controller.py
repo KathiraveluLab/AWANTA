c2b719ec41e7263759d49fb57c83c04f551fe291
Vamsi995 <saivamsi.ds123@gmail.com>
2024-06-29 14:17:20 +0530

    dp id based config

--- modules/emulator/controller.py
+++ modules/emulator/controller.py
@@ -1,3 +1,4 @@
+import copy
 import sys
 
 from ryu.base import app_manager
@@ -12,6 +13,9 @@ from ryu.lib import hub
 import json
 import os
 
+from ryu.topology import event
+from ryu.topology.api import get_switch, get_link, get_all_link
+
 
 class Controller(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -19,12 +23,14 @@ class Controller(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(Controller, self).__init__(*args, **kwargs)
         # monitor = Monitor()
-        self.datapath_ids = {}
+        self.datapath_ids = dict()
+        self.links = dict()
         self.rtt_matrix = [[-sys.maxsize for _ in range(3)] for _ in range(3)]
         self.network_graph = [[[0 for _ in range(3)] for _ in range(3)]]
         self.measurement_count = 0
-        self.link_to_index = {"s1":0, "s12": 1, "s2": 2}
-        self.ip_mapping = {"s1": "10.0.0.1", "s12": "10.0.0.2", "s2": "10.0.0.3"}
+        self.link_to_index = {1: 0, 12: 1, 2: 2}
+        self.src_ip = '10.0.0.1'
+        self.dst_ip = '10.0.0.2'
         self.index_to_link = {v:s for s, v in self.link_to_index.items()}
         self.latency_data = self.process_files()
         self.latency_monitor = hub.spawn(self.fetch_latency_results())
@@ -41,7 +47,7 @@ class Controller(app_manager.RyuApp):
         with open("modules/emulator/measurements/s12.json") as s12_m:
             s12_data = json.load(s12_m)
 
-        return {"s1": s1_data, "s12": s12_data, "s2": s2_data}
+        return {1: s1_data, 12: s12_data, 2: s2_data}
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
@@ -93,31 +99,68 @@ class Controller(app_manager.RyuApp):
 
         datapath.send_msg(mod_message)
 
+    """
+    The event EventSwitchEnter will trigger the activation of get_topology_data().
+    """
+    @set_ev_cls(event.EventSwitchEnter)
+    def handler_switch_enter(self, ev):
+        # The Function get_switch(self, None) outputs the list of switches.
+        self.topo_raw_switches = copy.copy(get_switch(self, None))
+        # The Function get_link(self, None) outputs the list of links.
+        self.topo_raw_links = copy.copy(get_link(self, None))
 
-    def set_ip_flow(self, datapath, source_dpid, destination_dpid):
-        datapath = datapath
+
+        for l in self.topo_raw_links:
+            link = l.to_dict()
+            src = link['src']
+            dst = link['dst']
+            self.links[src['dpid']] = self.links.get(src['dpid'], {})
+            self.links[src['dpid']][dst['dpid']] = int(src['port_no'])
+
+
+
+        """
+        Now you have saved the links and switches of the topo. So you could do all sort of stuf with them. 
+        """
+
+        # print(" \t" + "Current Links:")
+        # for l in self.topo_raw_links:
+        #     print(l.to_dict())
+        #     print (" \t\t" + str(l))
+        #
+        # print(" \t" + "Current Switches:")
+        # for s in self.topo_raw_switches:
+        #     t = s.to_dict()
+        #     print(int(t['dpid'], 16))
+        #     print (" \t\t" + str(s))
+
+    def set_ip_flow(self, source_dpid, destination_dpid):
+        print(self.datapath_ids)
+        print(source_dpid, destination_dpid)
+        datapath = self.datapath_ids[1]
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        srcip = self.ip_mapping[source_dpid]
-        dstip = self.ip_mapping[destination_dpid]
-
         match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                ipv4_src=srcip,
-                                ipv4_dst=dstip
+                                ipv4_src=self.src_ip,
+                                ipv4_dst=self.dst_ip
                                 )
-        # self.add_flow(datapath, 1, match, actions)
+        actions = parser.OFPActionOutput(self.links[source_dpid][destination_dpid])
+        self.add_flow(datapath, 1, match, actions)
 
 
     def fetch_latency_results(self):
-
-        print(os.getcwd())
+        hub.sleep(30)
 
         for dpid, latency_data in self.latency_data.items():
             ld = latency_data[self.measurement_count]
             self._update_rtt_matrix(dpid, ld)
-
         self.measurement_count += 1
+
+
+        dpids = self.get_optimal_route(1, 2)
+        self.set_optimal_route(dpids)
+
         hub.sleep(30)
 
 
@@ -126,16 +169,19 @@ class Controller(app_manager.RyuApp):
         source_index = self.link_to_index[source_dpid]
 
         for dpid, latency in latency_data.items():
-            target_index = self.link_to_index[dpid]
+            target_index = self.link_to_index[int(dpid)]
             self.rtt_matrix[source_index][target_index] = latency
 
-    def _set_optimal_route(self, source_dpid, target_dpid):
 
+    def get_optimal_route(self, source_dpid, target_dpid):
         source_index = self.link_to_index[source_dpid]
         target_index = self.link_to_index[target_dpid]
 
         direct_link = self.rtt_matrix[source_index][target_index]
         one_hop_node = None
+
+        output_dpids = []
+        output_dpids.append(source_dpid)
 
         for i in range(source_index, len(self.rtt_matrix)):
 
@@ -146,11 +192,18 @@ class Controller(app_manager.RyuApp):
                     # Update path specific link
                     one_hop_node = self.index_to_link[j]
 
-
         if one_hop_node != None:
-            self.set_ip_flow(self.datapath_ids[source_dpid[1:]], source_dpid, target_dpid)
+            output_dpids.append(one_hop_node)
+        output_dpids.append(target_dpid)
+
+        return output_dpids
 
 
+    def set_optimal_route(self, dpids):
+        print(dpids)
+        for i in range(len(dpids)-1):
+            self.set_ip_flow(dpids[i], dpids[i+1])
+            pass
 
 
 
