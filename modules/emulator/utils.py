cebf1820edf09ae0e337805fd4f2e2665836258e
Sai Vamsi Alisetti <vamsi@Vamsis-MacBook-Pro.local>
2024-07-12 00:25:06 +0530

    gitignore update

--- modules/emulator/utils.py
+++ modules/emulator/utils.py
@@ -3,7 +3,6 @@ from exceptions import ExtensionError
 
 
 def file_splitter(file_name: str) -> str:
-
     data = file_name.split(TraceManagerConstants.DELIMITER)
     filename = data[0]
     extension = data[1]
@@ -13,11 +12,12 @@ def file_splitter(file_name: str) -> str:
     else:
         raise ExtensionError
 
+
 def convert_dpid_key(dpid: str) -> int:
     return int(dpid, 16)
 
-def preprocess_ids(dpids: list[str]) -> list[str]:
 
+def preprocess_ids(dpids: list[str]) -> list[str]:
     for i in range(len(dpids)):
         if dpids[i] == MininetConstants.SRC_HOST or dpids[i] == MininetConstants.DST_HOST:
             continue
